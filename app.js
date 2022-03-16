d4957b66cf040b99361ad4e7c07e1c233c48d10a
Wesley <wesleystedman@gmail.com>
2022-03-15 17:26:02 -0700

    Add database config

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
+
+require('./config/database');
 
 app.use(logger('dev'));
 app.use(express.json());
